6622c7c27bbd909b5b6b9830b3ea19a00e898455
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-04-19 22:02:06 -0500

    chore: upgrade
    error with mongoose

--- packages/4/ms-4-nestjs/.eslintrc.js
+++ packages/4/ms-4-nestjs/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -16,6 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
